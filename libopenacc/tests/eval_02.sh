7d77c4f51ee3f655e44e66eb7ba4595da1e9f39a
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2013-12-03 15:49:52 -0500

    Working on evalutaion and modeling with MDCG.

--- libopenacc/tests/eval_02.sh
+++ libopenacc/tests/eval_02.sh
@@ -3,46 +3,52 @@
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib
 
-size=$((1024*1024))
+rm -f ref.log acc*-*-*.log
 
-for i in {1..10}
+for size in 20 23 27 30 35 39 44 50 57 64 72 81 90 101 113 126 140
 do
-#  echo "========================="
-#  echo "size   = $size"
-#  echo "-------------------------"
-#  echo -n "reference : "
-  echo -n "ref  "
-  ./cpu_ref_02 $size
 
-  gang=2
-  for j in {1..5}
+  echo "size = $size"
+
+  ./cpu_ref_02 $(($size*1000000)) >> ref.log
+
+  echo "----------------------------"
+
+  for gang in 1 2 4 8 16
   do
-#    echo "-------------------------"
-#    echo "gang   = $gang"
-
-    worker=2
-    for k in {1..6}
+    for worker in 4 8 16 32 64
     do
-#      echo "-------------------------"
-#      echo "worker = $worker"
-#      echo "-------------------------"
+      echo "size = $size"
+      echo "gang = $gang"
+      echo "worker = $worker"
 
-#      echo -n "version 1 : "
-      echo -n "acc1 "
-      ./test_02_v1 $size $gang $worker
-#      echo -n "version 2 : "
-      echo -n "acc2 "
-      ./test_02_v2 $size $gang $worker
-#      echo -n "version 3 : "
-      echo -n "acc3 "
-      ./test_02_v3 $size $gang $worker
+      ./test_02_v1 $(($size*1000000)) $gang $worker >> acc1-$gang-$worker.log
+      ./test_02_v2 $(($size*1000000)) $gang $worker >> acc2-$gang-$worker.log
+      ./test_02_v3 $(($size*1000000)) $gang $worker >> acc3-$gang-$worker.log
 
-      worker=$((2*$worker))
+      echo "----------------------------"
     done
-
-    gang=$((2*$gang))
   done
-
-  size=$((2*$size))
 done
+
+echo "" > plotter.gplot
+
+echo -n "plot" >> plotter.gplot
+
+for gang in 1 2 4 8 16 32
+do
+  for worker in 1 2 4 8 16 32 64 128
+  do
+    paste ref.log acc1-$gang-$worker.log | awk '{ print $1, $4, $5, $2/$6, $2/$7 }' > acc1-$gang-$worker.dat
+    echo -n " \"acc1-$gang-$worker.log\" using 1:4 title \"T0=? G=$gang T1=1 W=$worker T2=1\"," >> plotter.gplot
+
+    paste ref.log acc2-$gang-$worker.log | awk '{ print $1, $4, $5, $2/$6, $2/$7 }' > acc2-$gang-$worker.dat
+    echo -n " \"acc2-$gang-$worker.log\" using 1:4 title \"T0=1 G=$gang T1=? W=$worker T2=1\"," >> plotter.gplot
+
+    paste ref.log acc3-$gang-$worker.log | awk '{ print $1, $4, $5, $2/$6, $2/$7 }' > acc3-$gang-$worker.dat
+    echo -n " \"acc3-$gang-$worker.log\" using 1:4 title \"T0=1 G=$gang T1=1 W=$worker T2=?\"," >> plotter.gplot
+  done
+done
+
+gnuplot plotter.gplot
 
